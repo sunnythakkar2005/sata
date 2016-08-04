25e3c82547f2ea32d1dfdcd3f8a9dee067d4e498
sunny <sunnythakkar2005@gmail.com>
2016-08-04 23:24:21 +1000

    Initial Commit

---
+++ sataSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sataSwift
+//
+//  Created by Sunny Thakkar on 4/08/2016.
+//  Copyright © 2016 Sunny Thakkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
